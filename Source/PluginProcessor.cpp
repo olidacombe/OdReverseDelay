7ac33cf06b1d9c83bc4501ca60b8045957f68d50
Oli Dacombe <olidacombe@gmail.com>
2016-06-22 21:20:11 +0100

    processor get/setStateInformation populated a bit

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -300,12 +300,42 @@ void OdReverseDelayAudioProcessor::getStateInformation (MemoryBlock& destData)
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
+    XmlElement xml ("OdReverseDelaySettings");
+
+    // add some attributes to it..
+    //xml.setAttribute ("uiWidth", lastUIWidth);
+    //xml.setAttribute ("uiHeight", lastUIHeight);
+
+    // Store the values of all our parameters, using their param ID as the XML attribute
+    for (int i = 0; i < getNumParameters(); ++i)
+        if (AudioProcessorParameterWithID* p = dynamic_cast<AudioProcessorParameterWithID*> (getParameters().getUnchecked(i)))
+            xml.setAttribute (p->paramID, p->getValue());
+
+    // then use this helper function to stuff it into the binary blob and return it..
+    copyXmlToBinary (xml, destData);
 }
 
 void OdReverseDelayAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+    ScopedPointer<XmlElement> xmlState (getXmlFromBinary (data, sizeInBytes));
+
+    if (xmlState != nullptr)
+    {
+        // make sure that it's actually our type of XML object..
+        if (xmlState->hasTagName ("OdReverseDelaySettings"))
+        {
+            // ok, now pull out our last window size..
+            //lastUIWidth  = xmlState->getIntAttribute ("uiWidth", lastUIWidth);
+            //lastUIHeight = xmlState->getIntAttribute ("uiHeight", lastUIHeight);
+
+            // Now reload our parameters..
+            for (int i = 0; i < getNumParameters(); ++i)
+                if (AudioProcessorParameterWithID* p = dynamic_cast<AudioProcessorParameterWithID*> (getParameters().getUnchecked(i)))
+                    p->setValueNotifyingHost ((float) xmlState->getDoubleAttribute (p->paramID, p->getValue()));
+        }
+    }
 }
 
 //==============================================================================
